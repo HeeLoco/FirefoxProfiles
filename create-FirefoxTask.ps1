94b5b6c1b2dd2acae07bd2d159b3838dc47133f8
HeeLoco <66466597+HeeLoco@users.noreply.github.com>
2020-09-01 12:31:57 +0200

    Create create-FirefoxTask.ps1

---
+++ create-FirefoxTask.ps1
@@ -0,0 +1,38 @@
+#date 20.06.2020
+#author HeeLoco
+
+#I wrote the environment variables into my own variables to be flexible if they differ from the default configuration. e.g changing OneDrive (private) Path.
+#I also sometimes add parameters afterwards. 
+
+
+#get and set some vars 
+$myUserName = $env:USERNAME;
+$myTaskName = "Copy Firefox Profiles on LogON";
+
+$myPowerShellScriptPath = "C:\Users\" + $myUserName + "\OneDrive\some Configs and Profiles\Firefox\copy-FirefoxProfiles.ps1"
+$mySchtasksParamTR = "powershell.exe -ExecutionPolicy Bypass -File 'C:\Users\myusername\OneDrive\some Configs and Profiles\Firefox\copy-FirefoxProfiles.ps1'"
+
+#check for admin
+$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+if(!($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))){
+    Write-Error "Please run as Administrator!"
+    Start-Sleep -Seconds 5
+    return 666;
+}
+
+#test if script is avaiable
+if( !(Test-Path -Path $myPowerShellScriptPath -PathType Leaf)){
+    Write-Host "cannot find script";
+    return 666;
+}
+#make file always available? 
+
+#creating basic task 
+SCHTASKS /Create /TN $myTaskName /SC ONLOGON /TR $mySchtasksParamTR;
+
+#information
+Write-Host "Only a general task was created!`nPlease make the detailed settings yourself.`nTask Scheduler will opened now" -ForegroundColor Green;
+Start-Sleep -Seconds 5
+
+#start Task Scheduler GUI
+taskschd.msc
